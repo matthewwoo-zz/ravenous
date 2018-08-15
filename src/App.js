e28a03059a21f7dca9fa93e087dd3636ccc894f9
matthew.woo <matthew.edan.woo@gmail.com>
2018-08-15 06:28:51 -0700

    Added all the business components to App

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import SearchBar from './components/SearchBar/SearchBar';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
+class App extends React.Component {
   render() {
     return (
-      <div class="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
-    </div>
-  );
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
   }
 }
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -27,7 +27,7 @@ class SearchBar extends React.Component{
         <input placeholder="Where?" />
       </div>
       <div className="SearchBar-submit">
-        <a>Let"'"s Go </a>
+        <a>Lets Go </a>
       </div>
     </div>
     )
